bbc6de01a6fdb7b53b6fafd686fcda4854ffd694
muhscience <muhscience08@gmail.com>
2023-02-16 04:29:13 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
+. / betty - doc.pl 4 - puts.c
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
 	int main(void)
 {
 	const char *y = "Programming is like building a multilingual puzzle";
