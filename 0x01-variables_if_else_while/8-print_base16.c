c6b3b459ccc716f43935701acaf75b7b8003990f
muhscience <muhscience08@gmail.com>
2023-02-17 06:05:35 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+	/* print hexadecimals */
+	while (i < 16)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}		else
+			{
+				putchar(i - 10 + 'a');
+			}
+			i++;
+		}
+		putchar('\n');
+		return (0);
+
+}
