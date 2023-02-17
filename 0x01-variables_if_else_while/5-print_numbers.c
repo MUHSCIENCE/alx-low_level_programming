d8ce52edb8ed303df4c570561541d24128d02c3d
muhscience <muhscience08@gmail.com>
2023-02-17 05:15:58 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num;
+	 /* print single base 10 numbers */
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	return (0);
+}
