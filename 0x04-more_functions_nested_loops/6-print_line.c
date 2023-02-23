f6c89a140de0318c044579d75a2570b492a7e638
muhscience <muhscience08@gmail.com>
2023-02-23 05:35:51 -0800

    0 > 972?

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (int i = 1; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
