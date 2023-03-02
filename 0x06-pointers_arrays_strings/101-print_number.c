2915d9206884d48e0b372c55c21998192e48e448
MUHSCIENCE <muhscience08@gmail.com>
2023-03-02 05:28:22 -0800

    Advanced Task 9 real

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,12 +6,17 @@
  */
 void print_number(int n)
 {
+	unsigned int d;
+
+	d = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
-		if (n / 10)
-			print_number(n / 10);
-		_putchar(n % 10 + '0');
+		d = -n;
+	}
+		if (d / 10 != 0)
+		{
+			print_number(d / 10);
+		_putchar(d % 10 + '0');
 	}
 }
